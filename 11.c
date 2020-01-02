ff2263a4f88f7951b1e40816e1740c36ebd4d2f3
rajseelam <30663620+rajseelam@users.noreply.github.com>
2020-01-02 12:19:14 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            printf(" ");
+            
+        }
+        if(i==0||i==n-1)
+        {
+            for(j=0;j<n;j++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+        for(j=0;j<n;j++)
+        {
+            if(j==0||j==n-1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        }
+        printf("\n");
+    }
+}

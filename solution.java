b83ab176e114b7809d9dcd5fdea5378300e95d66
rajseelam <30663620+rajseelam@users.noreply.github.com>
2020-01-27 13:33:57 +0530

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class solution
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1=sc.nextLine();
+System.out.println(s+" "+"Technology"+" "+s1);
+}
+}

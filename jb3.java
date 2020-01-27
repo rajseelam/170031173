cbbf6c3dfde373b9823bd80865b7533fc06b8bf2
rajseelam <30663620+rajseelam@users.noreply.github.com>
2020-01-27 13:42:46 +0530

    Add files via upload

---
+++ jb3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class jb3
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+int s1=sc.nextInt();
+int s2=s+s1;
+System.out.println(s2);
+}
+}

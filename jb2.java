d6622995fab9775bbc9007e7f103513aa543c613
rajseelam <30663620+rajseelam@users.noreply.github.com>
2020-01-27 13:41:00 +0530

    Add files via upload

---
+++ jb2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class jb2
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+System.out.println("welcome"+" "+s);
+}
+}
